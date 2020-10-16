c1cc9a8c20cea6562c7438dcfb4d380e8afe3bba
Your Name <you@example.com>
2020-10-16 10:59:23 +0800

    xxx

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <map>
 #include <string.h>
 struct ListNode {
      int val;
@@ -44,6 +45,20 @@ ListNode* rotateRight(ListNode* head, int k) {
     tmp->next = head;
     return head_new;
 }
+//递归实现
+ListNode* rotateRight_recursive(ListNode* head, int k) {
+    ListNode* head_new = head;
+    ListNode* tail_new = head;
+    if(tail_new->next != nullptr && k > 0){
+        while(tail_new->next->next)
+            tail_new = tail_new->next;
+        head_new = tail_new->next;
+        head_new->next = head;
+        tail_new->next = nullptr;
+        head_new = rotateRight_recursive(head_new,k-1);
+    }
+    return head_new;
+}
 /************************
 给你一个字符串 S、一个字符串 T 。请你设计一种算法，可以在 O(n) 的时间复杂度内，从字符串 S 里面找出：包含 T 所有字符的最小子串。
 示例：
@@ -55,39 +70,69 @@ ListNode* rotateRight(ListNode* head, int k) {
 如果 S 中存在这样的子串，我们保证它是唯一的答案。
  * *********************/
 const char* minWindow(const char* S,const char* T){
-    const char *left = S,*right = S;
+    int left = 0,right = 0;
     const  char* dst = "",*dst_end = "";
     int match = 0;
     int needs['z'-'A'] = {0};
     int count['z'-'A'] = {0};
-    int needs_num = 0;
+    int match_num = 0;
     const char* p = T;
 
-    while(*p != '\0')
-    {
-        if(++needs[*p - 'A'] == 1)
-            needs_num++;
-        p++;
+    for(int i =0;i < strlen(T);i++){
+        if(++needs[T[i]-'A'] == 1)
+            match_num++;
     }
 
-    while(*right != '\0'){
-        count[*right - 'A']++;
-        if(count[*right - 'A'] == needs[*right - 'A'])
+    while(S[right] != '\0'){
+        count[S[right] - 'A']++;
+        if(count[S[right] - 'A'] == needs[S[right] - 'A'])
             match++;
         right++;
-        while(match == needs_num){
+        while(match == match_num){
             if(strcmp(dst,"") == 0 || right-left < dst_end-dst){
-                dst = left;
-                dst_end = right;
+                dst = S+left;
+                dst_end = S+right;
             }
-            if(--count[*left-'A'] < needs[*left-'A'])
+            if(--count[S[left]-'A'] < needs[S[left]-'A'])
                 match--;
             left++;
         }
     }
-    char* dst_new = (char*)malloc(dst_end-dst+1);
-    strncpy(dst_new,dst,dst_end-dst);
-    return dst_new;
+    return strndup(dst,dst_end-dst);
+}
+//c++
+const char* minWindow_cpp(const char* S,const char* T){
+    int left = 0,right = 0;
+    const  char* dst = "",*dst_end = "";
+    int match = 0;
+    std::map<char,int> needs;
+    std::map<char,int> count;
+    int needs_num = 0;
+    const char* p = T;
+
+    for(int i =0;i < strlen(T);i++)
+        needs[T[i]]++;
+
+    while(S[right] != '\0'){
+        if(needs.count(S[right]) == 1){
+            count[S[right]]++;
+            if(count[S[right]] == needs[S[right]])
+                match++;
+        }
+        right++;
+        while(match == needs.size()){
+            if(strcmp(dst,"") == 0 || right-left < dst_end-dst){
+                dst = S+left;
+                dst_end = S+right;
+            }
+            if(needs.count(S[left]) == 1){
+                if(--count[S[left]] < needs[S[left]])
+                    match--;
+            }
+            left++;
+        }
+    }
+    return strndup(dst,dst_end-dst);
 }
 int main(){
     ListNode* head = new ListNode(1);
@@ -96,9 +141,9 @@ int main(){
     ListAppend(head,4);
     ListAppend(head,5);
     ListPrint(head);
-    head = rotateRight(head,2);
+    head = rotateRight_recursive(head,2);
     ListPrint(head);
 
-    std::cout<<minWindow("ADOBECODEBANC",  "ABC")<<std::endl;
+    std::cout<<minWindow_cpp("ADOBECODEBANC",  "ABC")<<std::endl;
     return 0;
 }
